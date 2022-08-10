0758a200e30473c106cf6790ed2be9e189bfef95
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 17:36:48 -0500

    thought model complete

---
+++ models/Thought.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require('mongoose');
+// using moment.js to format timestamp
+const moment = require('moment');
+
+// Reaction will be subdocument of Thought
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },   
+    }
+);
+
+// this is for Thought schema
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
