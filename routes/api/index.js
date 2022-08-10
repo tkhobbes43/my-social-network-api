cebd838541cfe00d1ecb8505ca4fd70e41de3ef6
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 00:01:19 -0500

    did some psuedo coding and don't think i need friends and reaction routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
-const friendsRoutes = require('.friendsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtsRoutes);
-router.use('/friends', friendsRoutes);
 
 module.exports = router;
