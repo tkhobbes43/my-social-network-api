8e01f60a4046f5faae8861c6dc65152fb7b701a5
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 21:34:41 -0500

    got all thoughtController functions ready

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,25 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    
-}
+    getThoughts(req, res) {
+
+    },
+    getThoughtById(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    createReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    }
+};
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 
 const {
-    
-}
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
