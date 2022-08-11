117f0ef13fbd2d29e3a354ac3f2246350606881c
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 20:01:18 -0500

    completed getAllUsers and getUserById user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    // get a user by ID
+    getUserById(req,res) {
+        User.findOne({ _id: req.params.userId })
+        .populate('thoughts')
+        .populate('friends')
+        .select('-__v')
+        .then(async (user) => 
+            !user
+                ? res.status(404).json({ message: 'No user found with that ID'})
+                : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+    // 
+}
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,20 @@
 const router = require('expres').Router();
 
 // will import controller functions here
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users GET all and POST
+
+// /api/users/:userId GET, PUT, and DELETE by user's ID
+
+// /api/users/:userId/friends/:friendId POST and DELETE
+
+module.exports = router;
