ee63b31e2db80d3dd924c3262ddecef3cb4cfd88
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 22:19:39 -0500

    finished url routes for all the thoughtControllers that will be imported into thoughtRoutes.js

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -10,3 +10,13 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController');
 
+// /api/thoughts GET all thoughts and POST thought
+router.route('/').get(getThoughts).post(createThought);
+// /api/thoughts/:thoughtId GET, PUT, and DELETE a thought by id
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+// /api/thoughts/:thoughtId/reactions POST a reaction to a thought
+router.route('/:thoughtId/reactions').post(createReaction);
+// /api/thoughts/:thoughtId/reactions/:reactionId DELETE a reaction by id
+router.route('/:thought/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
