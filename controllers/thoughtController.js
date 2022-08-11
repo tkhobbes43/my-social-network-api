5742ff2671b78a37e7e37f6063d23b24c4119658
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 23:47:39 -0500

    finished up thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -68,10 +68,34 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+    // creating reaction and storing in single thought's reactions array field
     createReaction(req, res) {
-
+        console.log('You are adding an assignment');
+        console.log(req.body);
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought found with that ID!' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
+    // deleting reaction by its id
     deleteReaction(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought found with that ID!' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     }
 };
