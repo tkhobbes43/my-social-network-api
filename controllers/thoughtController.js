e65751617dbc61065e3c8e5e43121f1ff2661aff
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-10 22:30:08 -0500

    got getThoughts completed, starting on getThoughtById

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,11 +1,15 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+    // get all thoughts
     getThoughts(req, res) {
-
+        Thought.find({})
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
+    // get a thought by id
     getThoughtById(req, res) {
-
+        Thought.findOne({ })
     },
     createThought(req, res) {
 
